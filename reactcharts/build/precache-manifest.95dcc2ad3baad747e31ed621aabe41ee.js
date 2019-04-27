8467441dbd0beb65bf841163e7135f20a2c259e6
Joe Liao <joeliao@Joes-MacBook-Pro.local>
2019-04-27 16:16:00 -0400

    added build

---
+++ reactcharts/build/precache-manifest.95dcc2ad3baad747e31ed621aabe41ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721a69e6830d4edfb38f09bdb2374e01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1eca81ad9d47f42bab",
+    "url": "/static/css/main.e0031301.chunk.css"
+  },
+  {
+    "revision": "c3acd80271274148b068",
+    "url": "/static/js/2.826e8cd9.chunk.js"
+  },
+  {
+    "revision": "4a1eca81ad9d47f42bab",
+    "url": "/static/js/main.1042acd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
